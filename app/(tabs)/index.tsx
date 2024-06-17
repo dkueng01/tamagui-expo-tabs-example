c1730ed029dff95bcb5703a7c3507396bcb4b40c
dkueng <davidwichtig@gmx.at>
2024-06-17 20:15:06 +0200

    Init expo tabs code base with tamagui

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,12 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { Airplay } from "@tamagui/lucide-icons";
+import { Button, Heading, View } from "tamagui";
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    <View flex={1} alignItems="center" justifyContent="flex-start">
+      <Heading size={"$10"} color={"violet"}>
+        BIER ALARM
+      </Heading>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
