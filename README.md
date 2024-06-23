e4f00d14e857a831eaca8050e0b0c5f9dcdcac5f
dkueng <davidwichtig@gmx.at>
2024-06-23 17:02:38 +0200

    Small changes and readme tutorial link

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 * clone repo
 * npm install
 * npm run start
+
+[TUTORIAL](https://dev.to/hasan_py/setup-your-universal-app-with-react-native-expo-router-tamagui-and-storybook-28m6)
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,11 @@
 import { Platform } from "react-native";
 
 if (Platform.OS === "web") {
-  import("../tamagui-web.css");
+import("../tamagui-web.css");
 }
 
-import {
-  useFonts,
-  Inter_400Regular,
-  Inter_900Black,
-} from "@expo-google-fonts/inter";
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
+import { useFonts, Inter_400Regular, Inter_900Black } from "@expo-google-fonts/inter";
+import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
@@ -23,57 +15,53 @@ import { TamaguiProvider } from "tamagui";
 import tamaguiConfig from "@/tamagui.config";
 
 export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
+    // Catch any errors thrown by the Layout component.
+    ErrorBoundary,
 } from "expo-router";
 
 export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: "(tabs)",
+    // Ensure that reloading on `/modal` keeps a back button present.
+    initialRouteName: "(tabs)",
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [loaded, error] = useFonts({
+const [loaded, error] = useFonts({
     Inter: Inter_400Regular,
     InterBold: Inter_900Black,
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-  });
+});
 
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
+// Expo Router uses Error Boundaries to catch errors in the navigation tree.
+useEffect(() => {
     if (error) throw error;
-  }, [error]);
+}, [error]);
 
-  useEffect(() => {
+useEffect(() => {
     if (loaded) {
-      SplashScreen.hideAsync();
+        SplashScreen.hideAsync();
+        }
+    }, [loaded]);
+
+    if (!loaded) {
+        return null;
     }
-  }, [loaded]);
 
-  if (!loaded) {
-    return null;
-  }
-
-  return <RootLayoutNav />;
+    return <RootLayoutNav />;
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
+const colorScheme = useColorScheme();
 
-  return (
-    <TamaguiProvider
-      config={tamaguiConfig}
-      defaultTheme={colorScheme as string}
-    >
-      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+return (
+    <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme as string}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
         <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         </Stack>
-      </ThemeProvider>
+    </ThemeProvider>
     </TamaguiProvider>
-  );
+);
 }
